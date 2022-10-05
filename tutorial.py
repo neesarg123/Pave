642e159f1e584af2a5c511aeee58179389d581d3
neesarg123 <neesarg123@github.com>
2022-10-04 21:53:03 -0500

    need to fix to integers instead of text objects

--- tutorial.py
+++ tutorial.py
@@ -3,8 +3,8 @@ from manim import *
 
 class BinaryMultiplication(Scene):
     def construct(self):
-        cand_bits = [Text('1', color=BLUE), Text('0', color=BLUE), Text('1', color=BLUE), Text('0', color=BLUE)]
-        plier_bits = [Text('1', color=ORANGE), Text('1', color=ORANGE), Text('0', color=ORANGE), Text('1', color=ORANGE)]
+        cand_bits = [Integer(number=1, color=BLUE), Integer(number=0, color=BLUE), Integer(number=1, color=BLUE), Integer(number=0, color=BLUE)]
+        plier_bits = [Integer(number=1, color=ORANGE), Integer(number=1, color=ORANGE), Integer(number=0, color=ORANGE), Integer(number=1, color=ORANGE)]
         
         multiplicand = VGroup(cand_bits[0], cand_bits[1], cand_bits[2], cand_bits[3])        
         multiplier = VGroup(plier_bits[0], plier_bits[1], plier_bits[2], plier_bits[3])
@@ -48,18 +48,10 @@ class BinaryMultiplication(Scene):
         # Move the 'Multiplier' text down 
         self.play(plier_text.animate.shift(2.25*DOWN, 1.75*LEFT))
 
-        # Populate rest of the Product Bits with 0s
-        product_bits = [Text('0', color=PURPLE), Text('0', color=PURPLE), Text('0', color=PURPLE), Text('0', color=PURPLE)]
-        product_bits[0].next_to(product_rect, LEFT)
-        product_bits[0].shift(0.90*RIGHT).scale(0.78)
-
-        for i in range(1, 4):
-            product_bits[i].next_to(product_bits[i - 1], 2.85*RIGHT).scale(0.78)
-        
-        product_first_4 = VGroup(product_bits[0], product_bits[1], product_bits[2], product_bits[3])
+        # displaying product bits 
+        product_bits = [Integer(number=0, color=ORANGE), Integer(number=0, color=ORANGE), Integer(number=0, color=ORANGE), Integer(number=0, color=ORANGE)]
         self.play(Write(product_first_4))
-        
-        
+
         self.play(multiplicand.animate.shift(1.65*DOWN, 1.5*RIGHT).scale(1.3))
         self.play(multiplicand[0].animate.shift(1.2*LEFT), multiplicand[1].animate.shift(0.6*LEFT), 
                 multiplicand[2].animate.shift(0.05*LEFT), multiplicand[3].animate.shift(0.55*RIGHT),
@@ -67,23 +59,33 @@ class BinaryMultiplication(Scene):
 
         addition_sign = Text("+", color=YELLOW, font_size=30).next_to(product_rect, LEFT)
 
-        # if there is a 1, we add product (first 4) with multiplier (4 bits) AND THEN shift WHOLE Product to the right by 1 bit
-        # step 1: Add animation of multiplicand plus product's first four bits 
-        if multiplier[-1].text == "1":
-            # Frame the LSB of Product (Multiplier0)
-            frame_last_bit = SurroundingRectangle(multiplier[3], buff=.1, color=YELLOW)
-            self.play(Write(frame_last_bit))
-            self.play(Write(addition_sign))
+        for iteration in range(4):
+            # if there is a 1, we add product (first 4) with multiplier (4 bits) AND THEN shift WHOLE Product to the right by 1 bit
+            # step 1: Add animation of multiplicand plus product's first four bits 
+            if multiplier[-1].text == "1":
+                # Frame the LSB of Product (Multiplier0)
+                frame_last_bit = SurroundingRectangle(multiplier[3], buff=.1, color=YELLOW)
+                self.play(Write(frame_last_bit))
+                self.play(Write(addition_sign))
 
-            # step 2: Show the addition's answer 
-            cand_bin = multiplicand[0].text + multiplicand[1].text + multiplicand[2].text + multiplicand[3].text  
-            prod_bin = product_bits[0].text + product_bits[1].text + product_bits[2].text + product_bits[3].text
-            addition_result = list((bin(int(cand_bin,2) + int(prod_bin,2)))[2:])
+                # step 2: Show the addition's answer 
+                cand_bin = multiplicand[0] + multiplicand[1] + multiplicand[2] + multiplicand[3]
+                prod_bin = "".join([p.text for p in product_first_4[:4]])
+                addition_result = list((bin(int(cand_bin,2) + int(prod_bin,2)))[2:])
 
-            # step 3: Change the first four bits of the product 
-            for i in range(4):
-                # stopped here 
+                # step 3: Change the first four bits of the product 
+                for i in range(4):
+                    product_first_4[i].text = addition_result[i] 
 
-            self.play(Write(product_first_4))
+                self.play(FadeOut(product_first_4))
+                
+                self.play(Write(product_first_4))
+                self.play(FadeOut(frame_last_bit), FadeOut(multiplier[-1]), FadeOut(addition_sign))
+                # actually take out last multiplier bit since it's no longer needed
+                multiplier.remove(multiplier[-1])
 
-        # step 4: Shift the ENTIRE product grid by 1 bit to the right 
+            # step 4: Shift the ENTIRE product grid by 1 bit to the right 
+            # add 0 bit to the left 
+            product_first_4.insert(0, Text('0', color=PURPLE).next_to(product_first_4[0], 2.85*LEFT).scale(0.78))
+            prod_mul_group = VGroup(product_first_4, multiplier)
+            self.play(prod_mul_group.animate.shift(RIGHT))
